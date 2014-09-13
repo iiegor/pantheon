817a7e402a8b043b6648160c5828c941805e8df7
underc0de <dextrackmedia@gmail.com>
2014-09-13 16:15:49 +0200

    Added less support

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    less: {
+      development: {
+        files: {
+          'public/compiled/style.min.css': 'public/css/*.css.less'
+        }
+      }
+    },
+    watch: {
+      less: {
+        files: ['public/css/**/*.less'],
+        tasks: ['less:development']
+      }
+    },
+    shell: {
+      runServer: {
+        options: {
+          async: true
+        },
+        command: 'node app.js'
+      }
+    },
+    jshint: {
+      all: ['Gruntfile.js', 'public/js/**/*.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    jsbeautifier: {
+      modify: {
+        src: ['Gruntfile.js', 'public/js/**/*.js'],
+        options: {
+          config: '.jsbeautifyrc'
+        }
+      },
+      validate: {
+        src: ['Gruntfile.js', 'public/js/**/*.js'],
+        options: {
+          mode: 'VERIFY_ONLY',
+          config: '.jsbeautifyrc'
+        }
+      }
+    },
+    gettext_finder: {
+      files: ["views/*.html", "views/**/*.html"],
+      options: {
+        pathToJSON: ["locale/en_US/*.json"],
+        ignoreKeys: grunt.file.readJSON("gtf-ignored-keys.json")
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-shell-spawn');
+  grunt.loadNpmTasks('grunt-jsbeautifier');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['less', 'shell:runServer', 'watch']);
+
+  // Clean code before a commit
+  grunt.registerTask('clean', ['jsbeautifier:modify', 'jshint']);
+
+  // Validate code (read only)
+  grunt.registerTask('validate', ['jsbeautifier:validate', 'jshint']);
+
+  // Heroku
+  grunt.registerTask('heroku', ['less']);
+
+};
