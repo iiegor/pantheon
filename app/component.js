aa7b06ebc3041a27c9256877a5e823b6f00aab84
underc0de <iegormundo@hotmail.com>
2015-03-21 01:23:47 +0100

    Minimal fix

---
+++ app/component.js
@@ -0,0 +1,7 @@
+export default {
+  displayName: 'Component'
+
+  /*
+   * You can add default component functions
+   */
+};
--- app/components/IsomorphicComponent.js
+++ app/components/IsomorphicComponent.js
@@ -1,11 +1,13 @@
+import Component from '../component';
 import React from 'react/addons';
 
 export default React.createClass({
   displayName: 'IsomorphicComponent',
+  mixins: [Component],
 
   render: function() {
     return (
-      <p>Hello from a react component!</p>
+      <p>Hello from {this.constructor.displayName}!</p>
     );
   }
 });
