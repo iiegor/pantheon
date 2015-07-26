b99ce35124561b654dd0d079447b29040db97715
underc0de <iegormundo@hotmail.com>
2015-07-26 18:21:31 +0200

    Minimal cleanup

--- app.js
+++ app.js
@@ -1,40 +1,29 @@
 var colors = require('colors');
 console.log('Please wait while packages are loaded...'.gray);
 
-// ES6 Support
-var babel = require('babel/register');
-
 // Packages
-var habitat = require('habitat');
-var express = require('express');
-var nunjucks = require('nunjucks');
-var path = require('path');
-var fs = require('fs');
-var i18n = require('webmaker-i18n');
-var bodyParser = require('body-parser');
-var nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader(path.join(__dirname, '/app/views')));
-var birdy = require('./app/birdy');
+var habitat    = require('habitat'),
+    express    = require('express'),
+    nunjucks   = require('nunjucks'),
+    path       = require('path'),
+    fs         = require('fs'),
+    i18n       = require('webmaker-i18n'),
+    bodyParser = require('body-parser');
 
 // Prepare
 habitat.load();
 
 // Instantiate
-var app = express(),
-	env = new habitat('birdy');
-
-app.use(bodyParser.urlencoded({	extended: true	}));
-app.use(bodyParser.json());
+var app         = express(),
+    env         = new habitat('birdy'),
+    nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader(path.join(__dirname, '/app/views'), env.get('TEMPLATES_CACHE')));
 
 // Setup the application
 if ('prod' == env.get('ENV')) app.disable('verbose errors');
 
-nunjucksEnv.express(app);
-nunjucksEnv.addFilter('instantiate', function(input) {
-  var tmpl = new nunjucks.Template(input);
-  return tmpl.render(this.getVariables());
-});
-
-// Setup locales with i18n
+app.use(bodyParser.urlencoded({	extended: true	}));
+app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(i18n.middleware({
   supported_languages: env.get('SUPPORTED_LANGS'),
   default_lang: 'en-US',
@@ -42,15 +31,19 @@ app.use(i18n.middleware({
   translation_directory: path.resolve(__dirname, 'locales')
 }));
 
-// Static files
-app.use(express.static(path.resolve(__dirname, 'public')));
+nunjucksEnv.express(app);
+nunjucksEnv.addFilter('instantiate', function(input) {
+  var tmpl = new nunjucks.Template(input);
+
+  return tmpl.render(this.getVariables());
+});
 
 // Localized strings
 app.locals.languages = i18n.getSupportLanguages();
 app.get('/strings/:lang?', i18n.stringsRoute('en_US'));
 
 // Boot the application
-new birdy(app);
+require('babel/register'), new (require('./app/birdy'))(app);
 
 require('http').createServer(app).listen(env.get('SERVICE_PORT'), function() {
 	console.log('Birdy HTTP server is listening to %d (port) in %s (mode)!'.green, env.get('SERVICE_PORT'), env.get('ENV'));
