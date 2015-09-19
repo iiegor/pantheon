3067a5c49412431adb92454328f212f03e720d35
iiegor <iegormundo@hotmail.com>
2015-09-20 01:32:01 +0200

    :fire: Remove unnecessary deps

--- app/services/EmitterService.js
+++ app/services/EmitterService.js
@@ -1,32 +1,3 @@
-import emitter from 'component-emitter'
-import mout from 'mout'
+import {EventEmitter} from 'events'
 
-export default class EmitterService {
-
-	constructor() {
-		this._emitter = new emitter()
-	}
-
-	emit(event) {
-		this._emitter.emit(event)
-	}
-
-	on(event, callback) {
-		if(!callback) throw new Error('You must define an action for the event "' + event + '"')
-
-		this._emitter.on(event, callback)
-	}
-
-	register(events) {
-		// If not array break
-		if(!mout.lang.isArray(events)) throw new Error('Parameter "events" must be an array.')
-
-		// Needs a revision
-		var event;
-
-		for (event in events) {
-			this.on(event, events[event])
-		}
-	}
-
-}
+export default EventEmitter
