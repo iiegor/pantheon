59f18d96110f4a16118d8d074e63a24b3d75b9db
iiegor <iegormundo@hotmail.com>
2015-12-16 13:40:35 +0100

    Switch to dotenv

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var colors = require('colors')
 console.log('Please wait while packages are loaded...'.gray)
 
 // Packages
-var habitat    = require('habitat'),
+var dotenv    = require('dotenv'),
     express    = require('express'),
     nunjucks   = require('nunjucks'),
     path       = require('path'),
@@ -12,25 +12,24 @@ var habitat    = require('habitat'),
     compression   = require('compression')
 
 // Prepare
-habitat.load()
+dotenv.load()
 
 // Instantiate
 var app         = express(),
-    env         = new habitat('birdy'),
-    nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader(path.join(__dirname, '/app/views'), env.get('TEMPLATES_CACHE')))
+    nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader(path.join(__dirname, '/app/views'), process.env.TEMPLATES_CACHE))
 
 // Setup the application
 app.use(bodyParser.urlencoded({	extended: true	}))
 app.use(bodyParser.json())
 app.use(express.static(path.resolve(__dirname, 'public')))
 app.use(i18n.middleware({
-  supported_languages: env.get('SUPPORTED_LANGS'),
+  supported_languages: process.env.SUPPORTED_LANGS,
   default_lang: 'en-US',
   mappings: require('webmaker-locale-mapping'),
   translation_directory: path.resolve(__dirname, 'locales')
 }))
 
-if ('prod' == env.get('ENV')) {
+if ('prod' == process.env.BIRDY_ENV) {
   app.disable('verbose errors')
   app.use(compression())
 }
@@ -51,13 +50,13 @@ app.get('/strings/:lang?', i18n.stringsRoute('en_US'))
 // Boot the application
 require('babel/register'), new (require('./app/birdy'))(app)
 
-require('http').createServer(app).listen(env.get('SERVICE_PORT'), function() {
-	console.log('Birdy HTTP server is listening to %d (port) in %s (mode)!'.green, env.get('SERVICE_PORT'), env.get('ENV'));
+require('http').createServer(app).listen(process.env.BIRDY_PORT, function() {
+	console.log('Birdy HTTP server is listening to %d (port) in %s (mode)!'.green, process.env.BIRDY_PORT, process.env.BIRDY_ENV);
 })
 
-if (env.get('HTTPS_ENABLED')) require('https').createServer({
-	key: fs.readFileSync(env.get('HTTPS_KEY')),
-	cert: fs.readFileSync(env.get('HTTPS_CERT'))
-}, app).listen(env.get('HTTPS_PORT'), function() {
-	console.log('Birdy HTTPS server is listening to %d (port) in %s (mode) with https enabled!'.green, env.get('HTTPS_PORT'), env.get('ENV'))
+if (process.env.HTTPS_ENABLED) require('https').createServer({
+	key: process.env.HTTPS_KEY,
+	cert: process.env.HTTPS_CERT
+}, app).listen(process.env.HTTPS_PORT, function() {
+	console.log('Birdy HTTPS server is listening to %d (port) in %s (mode) with https enabled!'.green, process.env.HTTPS_PORT, process.env.BIRDY_ENV)
 })
